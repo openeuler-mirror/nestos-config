bd69f52e24e12b10436a5fa2c92b9b2ab4b26fb9
duyiwei <duyiwei@kylinos.cn>
2023-09-12 11:04:04 +0800

    overlay.d/05core/usr/lib/dracut/modules.d/35coreos-live/coreos-live-unmount-tmpfs-var.service

--- overlay.d/05core/usr/lib/dracut/modules.d/35coreos-live/module-setup.sh
+++ overlay.d/05core/usr/lib/dracut/modules.d/35coreos-live/module-setup.sh
@@ -32,19 +32,19 @@ install() {
     inst_simple "$moddir/live-generator" \
         "$systemdutildir/system-generators/live-generator"
 
-    inst_simple "$moddir/coreos-live-unmount-tmpfs-var.sh" \
-        "/usr/sbin/coreos-live-unmount-tmpfs-var"
+    inst_simple "$moddir/nestos-live-unmount-tmpfs-var.sh" \
+        "/usr/sbin/nestos-live-unmount-tmpfs-var"
 
     inst_simple "$moddir/coreos-livepxe-rootfs.sh" \
         "/usr/sbin/coreos-livepxe-rootfs"
 
-    install_and_enable_unit "coreos-live-unmount-tmpfs-var.service" \
+    install_and_enable_unit "nestos-live-unmount-tmpfs-var.service" \
         "initrd-switch-root.target"
 
     install_and_enable_unit "coreos-livepxe-rootfs.service" \
         "initrd-root-fs.target"
 
-    install_and_enable_unit "coreos-live-clear-sssd-cache.service" \
+    install_and_enable_unit "nestos-live-clear-sssd-cache.service" \
         "ignition-complete.target"
 
     install_and_enable_unit "coreos-liveiso-persist-osmet.service" \
